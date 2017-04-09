b7b7f5f23c499d264979f257b55f975d01ad6d12
Kevin <gkangkai@gmail.com>
2017-04-09 22:58:33 +0800

    server side framework 2

---
+++ avalon-core.js
@@ -0,0 +1,17 @@
+class AvalonCore {
+    constructor (player_num) {
+        this.player_num = player_num;
+    }
+
+    init () {
+        // TODO
+    }
+
+    operate () {
+        // TODO
+    }
+
+    exit () {
+        // TODO
+    }
+}
---
+++ public/js/index.js
@@ -0,0 +1,23 @@
+const Status = {
+    Idle: 0,
+    Wait: 1,
+    Play: 2,
+};
+
+socket = io();
+
+socket.on('connection', function (data) {
+    // TODO
+});
+
+socket.on('login', function (data) {
+    // TODO
+});
+
+socket.on('operate', function (data) {
+    // TODO
+});
+
+socket.on('msg', function (data) {
+    // TODO
+});
