4ff53e428758dff7ffa9d1e12289e6962e3adaa6
KevinK <gkangkai@gmail.com>
2017-10-24 23:59:49 +0800

    RoomManage

--- src/RoomManage.ts
+++ src/RoomManage.ts
@@ -1,16 +1,37 @@
 import IGameMachine from './IGameMachine'
+import AvalonMachine from './AvalonMachine'
+
+class Seat
+{
+
+}
 
 export class Room
 {
     id: string;
+    num: number;
     machine: IGameMachine;
     
-    constructor (id: string) {
-    
+    constructor (id: string, num: number) {
+        this.id = id;
+        this.num = num;
+    }
+
+    private start() {
+        this.machine = new AvalonMachine(this.num);
+        this.machine.Start();
     }
 }
 
 export default class RoomManager
 {
-    rooms: MapConstructor;
+    rooms: Map<string, Room>;
+
+    constructor() {
+        this.rooms = new Map<string, Room>();
+    }
+
+    GetRoom(id: string): Room {
+        return this.rooms[id];
+    }
 }
---
+++ src/views/main.ts
@@ -0,0 +1,22 @@
+enum ROLE {
+    Merlin,
+    Percival,
+    Loyalist,
+    Assassin,
+    Morgana,
+    Mordred,
+    Oberon,
+    Minion,
+    // Lancelot: 8, 9,
+}
+
+enum STATUS {
+    Wait,
+    MakeTeam,
+    TeamVote,
+    TaskVote,
+    Assassin,
+    End,
+}
+
+var GameVM;
